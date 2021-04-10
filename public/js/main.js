3f19432275194b731b3320b9b3cf3233f1dc29fe
gunit <shopifydevgk@gmail.com>
2021-04-10 19:37:51 +0300

    Added console log for weather search input

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,8 @@ form.addEventListener('submit', (e) => {
   const input = document.querySelector('.js-input')
   const inputVal = input.value
 
+  console.log(inputVal);
+
   fetch('http://localhost:4000/weather?address=' + inputVal).then((response) => {
     response.json().then((data) => {
       if (data.error) {
