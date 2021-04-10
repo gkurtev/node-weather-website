1e6d02cd0a63cd015f89f51251eef40b5dffa784
gunit <shopifydevgk@gmail.com>
2021-04-10 20:08:33 +0300

    Fixed missing element problem

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,23 @@
 
 const form = document.querySelector('.js-weather-form')
+if (form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+    const input = document.querySelector('.js-input')
+    const inputVal = input.value
 
-  const input = document.querySelector('.js-input')
-  const inputVal = input.value
+    console.log(inputVal);
 
-  console.log(inputVal);
-
-  fetch('/weather?address=' + inputVal).then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        console.log('error');
-      } else {
-        console.log(data);
-      }
+    fetch('/weather?address=' + inputVal).then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          console.log('error');
+        } else {
+          console.log(data);
+        }
+      })
     })
   })
-})
+}
+
